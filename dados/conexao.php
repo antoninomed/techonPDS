6f5dfb4a8689f829273b51fbb86808fc393f6fa9
Mangueira <52980921+e-Mangueira@users.noreply.github.com>
2022-01-03 20:00:57 +0000

    Alterou conexão

--- dados/conexao.php
+++ dados/conexao.php
@@ -4,7 +4,7 @@ class Conexao {
   private static $instancia;
   public static function getConexao(){
       if (!isset(self::$instancia)):
-          self::$instancia = new PDO ("mysql:host=localhost;dbname=techonbd","root","123");
+          self::$instancia = new PDO ("mysql:host=localhost;dbname=techonbd","root","");
       endif;
       return self::$instancia;
   }    
