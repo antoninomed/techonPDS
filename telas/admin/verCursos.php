f5b9f6871670b0035602c9573c5e7c8af7495cb7
Mangueira <52980921+e-Mangueira@users.noreply.github.com>
2022-01-06 17:27:19 -0300

    Add files via upload

--- telas/admin/verCursos.php
+++ telas/admin/verCursos.php
@@ -10,8 +10,6 @@ include ('../../dados/conexao.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="admin.css">
-
-
 </head>
 <body>
 <main>    
