d7855f26d3190b7a124000e61bfa31c74b80c679
Mangueira <52980921+e-Mangueira@users.noreply.github.com>
2022-01-06 17:27:55 -0300

    Add files via upload

--- telas/professor/cadastrarCursos.php
+++ telas/professor/cadastrarCursos.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-    <?php include('../../dados/sessao.php'); ?>
+    <?php include('../../dados/sessao.php'); 
+    require ('home.php') ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,28 +12,7 @@
     
     <title>Administrador techON</title>
 </head>
-<body>
-    <header>
-    <nav class="navbar sticky-top navbar-expand-lg">
-            <a href="home.php" class="logo-cabecalho">techON</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="verCursos.php" class="nav-link">Ver Cursos</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="cadastrarCursos.php" class="nav-link">Cadastrar Cursos</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a href="../logout.php" class="nav-link">Sair</a>
-                        </li> 
-                          
-                    </ul>
-
-        </nav>   
-    </header>
-    
+<body>       
     <main>
         <div class="info-pag" style="font-family: 'Blinker', sans-serif;">
             <h6 style="color:#1f5ebb">Curso</h6>    
--- telas/professor/verCursos.php
+++ telas/professor/verCursos.php
@@ -1,38 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-    <?php include('../../dados/sessao.php');?>
+    <?php include('../../dados/sessao.php');
+    require ('home.php')?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../css/cabecalho.css">
     <link rel="stylesheet" href="professor.css">
     
     <title>Administrador techON</title>
 </head>
-<body>
-    <header>
-    <nav class="navbar sticky-top navbar-expand-lg">
-            <a href="home.php" class="logo-cabecalho">techON</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="verCursos.php" class="nav-link">Ver Cursos</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="cadastrarCursos.php" class="nav-link">Cadastrar Cursos</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a href="../logout.php" class="nav-link">Sair</a>
-                        </li> 
-                          
-                    </ul>
-
-        </nav>   
-    </header>
-    
+<body>    
     <main>
         <div class="info-pag" style="font-family: 'Blinker', sans-serif;">
             <h6 style="color:#1f5ebb">Cursos Cadastrados</h6>    
@@ -40,7 +18,6 @@
             <hr>
         </div>
         <container class="list-dados" style="display:flex; margin-left: 4vw; flex-wrap:wrap;">
-        <ul>
             <?php
             include('cursoDAO.php');
 
@@ -70,7 +47,7 @@
     </container>
     <script data-require="jquery@*" data-semver="2.0.3" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
     <script data-require="bootstrap@*" data-semver="3.1.1" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <link data-require="bootstrap-css@3.1.1" data-semver="3.1.1" rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="exclusao.js"></script>  
     
     </main>
