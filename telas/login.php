9112550cbc765d1178b086dbbbd08e62235e3271
Mangueira <52980921+e-Mangueira@users.noreply.github.com>
2022-01-06 17:28:10 -0300

    Add files via upload

--- telas/login.php
+++ telas/login.php
@@ -14,13 +14,22 @@ require ('cabecalho.php');
 </head>
 <body>
 
+
     <main id="main-login">
         <container class="container-login">  
             <img class = "img-central" src="../imagens/img-inicio.png" alt=""> 
             <form class = "form-central" action="../dados/validacaoLogin.php" method="get">	
                 <h5>Insira seu e-mail e senha para entrar.</h5>
                 <input type="text" name="email" placeholder="E-mail" id="email" class="caixa" autofocus/> <br/>
-                <input type="password" name="senha" placeholder="Senha" id="senha" class="caixa"/> <br/>           
+                <input type="password" name="senha" placeholder="Senha" id="senha" class="caixa"/> <br/>  
+                <?php
+                if(isset($_SESSION['idError'])){
+                    if($_SESSION['idError']==1){?>
+                        <p class="erro">Usuário ou senha inválida.</p><?php
+                        }		
+                    }
+                unset($_SESSION['idError']);
+                ?>     
                 <input class = "botaoEntrar" type="submit" value="Entrar" />
                 <p class="cadastro">Não possui conta?<a href="cadastro.php"> Cadastre-se aqui</a></p>                      
             </form>
